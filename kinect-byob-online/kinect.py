3d47168d0a5b2f12a70929883fe10221038f5dee
Michael Lee <michael.lee.0x2a@gmail.com>
2014-09-16 09:50:31 -0700

    Cleaned up code

--- kinect-byob-online/kinect.py
+++ kinect-byob-online/kinect.py
@@ -1,13 +1,20 @@
-"""provides a simple PyGame sample with video, depth stream, and skeletal tracking"""
+#!/usr/bin/env/python
+"""
+Provides support for interfacing with the kinect via pykinect and providing
+skeletal data.
+"""
 
-import pykinect
+from __future__ import print_function, division
+
 from pykinect import nui
 from pykinect.nui import JointId
 
-import time 
+import time
 import multiprocessing
 
-WIDTH = 240 * 2 
+# BYOB screen's coordinate system starts from -240 to 240 along the x axis, and
+# -180 to 180 along the y axis.
+WIDTH = 240 * 2
 HEIGHT = 180 * 2
 
 JOINTS = {
@@ -32,132 +39,159 @@ JOINTS = {
     'wristleft': JointId.WristLeft,
     'wristright': JointId.WristRight
 }
-        
+
+
+def normalize(pos):
+    '''Normalizes the Kinect's coordinate system to BYOB coordinates.
+    Returns 'z' as the the distance of the joint in millimeters from the
+    Kinect. Returns the 'w' value unchanged.'''
+    output = nui.SkeletonEngine.skeleton_to_depth_image(pos, WIDTH, HEIGHT)
+    return {
+        'x': output[0] - WIDTH // 2,
+        'y': -(output[1] - HEIGHT // 2),
+        'z': pos.z * 1000,
+        'w': pos.w
+    }
+
+
 class KinectProcess(multiprocessing.Process):
+    '''Launches a separate process which monitors the Kinect and updates
+    its internal data with each frame update. To start the process, call
+    the 'start' method; to end it call the 'join' method.'''
     def __init__(self, data):
+        '''"data" must be a 'multiprocessing.Manager.dict' object, and
+        is used to synchronize data across different processes.
+        '''
         super(KinectProcess, self).__init__(name='KinectProcess')
         self.data = data
         self.data['num_tracked'] = 0
-            
-    def _is_valid(self, skeleton):
-        head_pos = skeleton.SkeletonPositions[JointId.Head]
-        return head_pos.x != 0 or head_pos.y != 0
-        
-    def _normalize(self, pos):
-        output = nui.SkeletonEngine.skeleton_to_depth_image(pos, WIDTH, HEIGHT)
-        return {
-            'x': int(output[0] - WIDTH // 2), 
-            'y': -int(output[1] - HEIGHT // 2), 
-            'z': pos.z, 
-            'w': pos.w 
-        }
-        
-    def _set_data(self, index, skeleton):
+
+    def _set_data(self, skeleton_number, skeleton):
+        '''Synchronizes the skeleton data'''
         for name, joint_id in JOINTS.items():
-            key = str(index) + name
+            key = str(skeleton_number) + name
             pos = skeleton.SkeletonPositions[joint_id]
-            normalized = self._normalize(pos)
+            normalized = normalize(pos)
             for coord in 'xyzw':
+                # Note: since `self.data` is not a normal dict, it isn't
+                # save to include nested data structures since they won't
+                # be correctly synchronized across processes.
+                #
+                # Use `self.data` as a key-value store only.
                 self.data[key + coord] = normalized[coord]
-            
+
     def run(self):
+        '''Sets up the kinect data and begins watching for updates.'''
         def display(frame):
+            '''Will be called every time the Kinect has a new frame.
+            Processes and synchronizes that data.'''
             index = 0
+            tracked_enum = nui.SkeletonTrackingState.TRACKED
             for skeleton in frame.SkeletonData:
-                if skeleton.eTrackingState == nui.SkeletonTrackingState.TRACKED:
+                if skeleton.eTrackingState == tracked_enum:
                     index += 1
                     self._set_data(index, skeleton)
             self.data['num_tracked'] = index
-        
+
         with nui.Runtime() as kinect:
             kinect.skeleton_engine.enabled = True
             kinect.skeleton_frame_ready += display
-            
+
             kinect.video_stream.open(
-                nui.ImageStreamType.Video, 
-                2, 
-                nui.ImageResolution.Resolution640x480, 
+                nui.ImageStreamType.Video,
+                2,
+                nui.ImageResolution.Resolution640x480,
                 nui.ImageType.Color)
             kinect.depth_stream.open(
-                nui.ImageStreamType.Depth, 
-                2, 
-                nui.ImageResolution.Resolution320x240, 
+                nui.ImageStreamType.Depth,
+                2,
+                nui.ImageResolution.Resolution320x240,
                 nui.ImageType.Depth)
-        
+
             while True:
                 pass
-            
+
+
 class KinectData(object):
+    '''A wrapper object providing better support for retrieving data
+    from the Kinect process'''
     def __init__(self):
-        pass 
-        
-    def start(self):
         self.manager = multiprocessing.Manager()
         self.data = self.manager.dict()
         self.process = KinectProcess(self.data)
+
+    def start(self):
+        '''Starts the underlying Kinect process.'''
         self.process.start()
-        
+
     def end(self):
+        '''Ends the underlying Kinect process.'''
         self.process.join()
-        
+
     def match(self, skeleton_number='', joint='', coord=''):
+        '''Returns all joint data that corresponds to the provided
+        skeleton, joint, and coord. Will perform a case-insensitive match.
+
+        Example:
+
+            >>> kinect_data.match(1, 'HandLeft', 'x')
+            {
+                '1handleftx': 32
+            }
+
+            >>> kinect_data.match(1, 'HandLeft')
+            {
+                '1handleftx': 32,
+                '1handlefty': -100.3,
+                '1handleftz': 821,
+                '1handleftw': 1.0
+            }
+        '''
         match = '{0}{1}{2}'.format(skeleton_number, joint, coord)
         match = match.lower().replace('_', '')
-            
-        output = []
-        for key, value in self.data.items():
-            if key.startswith(match) and key != 'num_tracked':
-                output.append('{0}={1}'.format(key, value))
-        return '\n'.join(output)
-        
+        match_func = lambda k: match in k and k != 'num_tracked'
+
+        return {k: v for k, v in self.data.items() if match_func(k)}
+
     def get(self, key):
+        '''Retrieves the data associated with the provided key
+        (case-insensitive). If no data is associated with the key,
+        returns None.
+
+        Example:
+
+            >>> kinect_data.get('1handleftx')
+            32
+
+        '''
         key = key.lower().replace('_', '')
         if key in self.data:
             return self.data[key]
         else:
-            return ''
-    
+            return None
+
     def get_num_tracked(self):
+        '''Returns the number of skeletons currently being tracked.'''
         return self.data['num_tracked']
-        
-def main2():
-    kinect_data = KinectData()
-    kinect_data.start()
+
+
+def main():
+    '''Launches just the Kinect process, and prints out the head coordinate
+    data of the first player for debugging purposes.'''
     try:
+        print("Starting Kinect process")
+        kinect_data = KinectData()
+        kinect_data.start()
+
+        print("Ready")
         while True:
             time.sleep(1)
+            print(kinect_data.match(1, 'Head'))
+    except KeyboardInterrupt:
+        kinect_data.end()
     except:
         kinect_data.end()
         raise
-  
-def main():
-    skeletons = None
-    
-    def post_frame(frame):
-        print "??"
-        inspect_skeletons(frame.SkeletonData)
-        
-    def inspect_skeletons(skeletons):
-        print "!!"
-        for index, data in enumerate(skeletons):
-            # draw the Head
-            HeadPos = data.SkeletonPositions[JointId.Head]
-            print HeadPos
-
-            
-  
-    kinect = nui.Runtime()
-    
-    kinect.skeleton_engine.enabled = True
-    kinect.skeleton_frame_ready += post_frame
-    
-    kinect.video_stream.open(nui.ImageStreamType.Video, 2, nui.ImageResolution.Resolution640x480, nui.ImageType.Color)
-    kinect.depth_stream.open(nui.ImageStreamType.Depth, 2, nui.ImageResolution.Resolution320x240, nui.ImageType.Depth)
-
-    print "ready"
-    
-    while True:
-        time.sleep(1)
 
 if __name__ == '__main__':
-    main2()
+    main()
--- kinect-byob-online/server.py
+++ kinect-byob-online/server.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+'''Runs the webserver and serves the Kinect data from port 5000.'''
 
 from __future__ import print_function, division
 
@@ -7,47 +8,59 @@ from flask.ext.cors import CORS
 
 import kinect
 
-app = Flask(__name__)
-cors = CORS(app)
+def json_to_key_value(json):
+    return '\n'.join('{}={}'.format(*pair) for pair in json.items())
 
-kinect_data = kinect.KinectData()
 
-@app.route("/")
-def index():
-    return str(kinect_data.data)
+def setup():
+    app = Flask(__name__)
+    CORS(app)
+    kinect_data = kinect.KinectData()
 
-@app.route("/skeletons")
-def skeletons():
-    return kinect_data.match()
-    
-@app.route("/skeletons/<int:skeleton_number>")
-def skeleton(skeleton_number):
-    return kinect_data.match(skeleton_number)
-    
-@app.route("/skeletons/<int:skeleton_number>/<joint>")
-def skeleton_joint(skeleton_number, joint):
-    return kinect_data.match(skeleton_number, joint)
-    
-@app.route("/skeletons/<int:skeleton_number>/<joint>/<coord>")
-def skeleton_joint_coord(skeleton_number, joint, coord):
-    return kinect_data.match(skeleton_number, joint, coord)
-    
-@app.route("/num_tracked")
-def num_tracked():
-    return str(kinect_data.get_num_tracked())
-    
-@app.route("/joint/<target>")
-def joint(target):
-    a = str(kinect_data.get(target))
-    print('>' + a + '<')
-    return a
-    
+    @app.route("/")
+    def index():
+        return json_to_key_value(kinect_data.data)
+
+
+    @app.route("/skeletons")
+    def skeletons():
+        return json_to_key_value(kinect_data.match())
+
+
+    @app.route("/skeletons/<int:skeleton_number>")
+    def skeleton(skeleton_number):
+        return json_to_key_value(kinect_data.match(skeleton_number))
+
+
+    @app.route("/skeletons/<int:skeleton_number>/<joint>")
+    def skeleton_joint(skeleton_number, joint):
+        return json_to_key_value(kinect_data.match(skeleton_number, joint))
+
+
+    @app.route("/skeletons/<int:skeleton_number>/<joint>/<coord>")
+    def skeleton_joint_coord(skeleton_number, joint, coord):
+        return json_to_key_value(kinect_data.match(skeleton_number, joint, coord))
+
+
+    @app.route("/num_tracked")
+    def num_tracked():
+        return str(kinect_data.get_num_tracked())
+
+
+    @app.route("/joint/<target>")
+    def joint(target):
+        data = kinect_data.get(target)
+        return str(data) if data is not None else ''
+
+    return app, kinect_data
+
 def main():
+    app, kinect_data = setup()
     try:
         kinect_data.start()
         app.run(debug=True, use_reloader=False)
     except KeyboardInterrupt:
         kinect_data.end()
-        
+
 if __name__ == '__main__':
     main()
